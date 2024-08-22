a1bb04183349ec1a05893897cff988c191b11db8
Elvis Ngatia <ngatiadyte@gmail.com>
2024-08-22 10:23:10 +0300

    chore: update web url

--- bin/web_scraper.dart
+++ bin/web_scraper.dart
@@ -3,7 +3,7 @@ import 'package:web_scraper/web_scraper.dart' as web_scraper;
 
 void main() async {
   print('___Web Scraper___');
-  final baseUrl = 'https://beautytargetkenya.com/collections/all';
+  final baseUrl = 'https://beautytargetke.com/collections/all';
   // await web_scraper.prisma.$connect();
   final int totalPages = 10;
   for (int page = 1; page < totalPages+1; page++) {
